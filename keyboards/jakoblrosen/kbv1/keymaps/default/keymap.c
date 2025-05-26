465ee9fa60e468d7f65471ad718da65c0161e063
jakoblrosen <jake3.rosen@gmail.com>
2025-05-26 17:25:59 -0600

    config for breadboard poc kb

---
+++ keyboards/jakoblrosen/kbv1/keymaps/default/keymap.c
@@ -0,0 +1,6 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_A, KC_B, KC_C, KC_LSFT, KC_0, KC_1, KC_2, KC_3)};
